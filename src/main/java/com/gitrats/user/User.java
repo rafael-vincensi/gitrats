f98d7871910702de0609a56962cb07d786fde367
rafael-vincensi <rafaelvincensi.m@gmail.com>
2026-09-02 20:08:16 -0300

    feat(user): adiciona model inicial de user

---
+++ src/main/java/com/gitrats/user/User.java
@@ -0,0 +1,36 @@
+package com.gitrats.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table (name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column (unique = true)
+    private String username;
+    private String displayName;
+    private String bio;
+
+    @Column(columnDefinition = "TEXT")
+    private String profilePicture;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+
+}
