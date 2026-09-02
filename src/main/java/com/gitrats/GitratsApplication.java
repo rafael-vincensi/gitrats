9b6d9ef88ad4be62d5c791725bd58a3e7022e36e
rafael-vincensi <rafaelvincensi.m@gmail.com>
2026-09-02 19:27:41 -0300

    Commit inicial

---
+++ src/main/java/com/gitrats/GitratsApplication.java
@@ -0,0 +1,13 @@
+package com.gitrats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitratsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitratsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitrats/GitratsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitrats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitratsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
