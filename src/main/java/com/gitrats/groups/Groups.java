e8c5b31ec9caf11118b755a695e32432ba3c8f6a
rafael-vincensi <rafaelvincensi.m@gmail.com>
2026-09-03 19:29:24 -0300

    feat(groups): cria entidade groups

---
+++ src/main/java/com/gitrats/groups/Groups.java
@@ -0,0 +1,32 @@
+package com.gitrats.groups;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "groups")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Groups {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(name = "code", unique = true, nullable = false, length = 6)
+    private String code;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
