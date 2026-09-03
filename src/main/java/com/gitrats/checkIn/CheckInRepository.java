1eb9f12285d0c7f6da70f8ea9028923de60fad81
rafael-vincensi <rafaelvincensi.m@gmail.com>
2026-09-02 21:00:43 -0300

    feat(checkin): cria repository de checkin

---
+++ src/main/java/com/gitrats/checkIn/CheckInRepository.java
@@ -0,0 +1,8 @@
+package com.gitrats.checkIn;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CheckInRepository extends JpaRepository<CheckIn, Long> {
+}
