7e3840a70da0eb06445a389801596c71ce8a8522
rafael-vincensi <rafaelvincensi.m@gmail.com>
2026-09-03 00:07:05 -0300

    fix(user): atualiza id para Long

--- src/main/java/com/gitrats/user/User.java
+++ src/main/java/com/gitrats/user/User.java
@@ -15,8 +15,8 @@ import java.util.UUID;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @Column (unique = true)
     private String username;
