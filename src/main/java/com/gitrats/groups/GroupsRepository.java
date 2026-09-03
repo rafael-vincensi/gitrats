f09b923dbb5bac1165aac24c56f1b0099718de2b
rafael-vincensi <rafaelvincensi.m@gmail.com>
2026-09-03 19:30:16 -0300

    feat(groups): cria repository de groups

---
+++ src/main/java/com/gitrats/groups/GroupsRepository.java
@@ -0,0 +1,7 @@
+package com.gitrats.groups;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface GroupsRepository extends JpaRepository <Groups, Long> {
+}
