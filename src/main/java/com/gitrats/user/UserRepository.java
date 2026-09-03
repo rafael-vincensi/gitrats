e21ea83d408d57cf10b13a55b57becd8c492fee1
rafael-vincensi <rafaelvincensi.m@gmail.com>
2026-09-03 00:26:11 -0300

    feat(user): cria metodo pra buscar usuario por username

--- src/main/java/com/gitrats/user/UserRepository.java
+++ src/main/java/com/gitrats/user/UserRepository.java
@@ -2,7 +2,9 @@ package com.gitrats.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
+import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository <User, Long> {
+    Optional<User> findByUsername(String username);
 }
