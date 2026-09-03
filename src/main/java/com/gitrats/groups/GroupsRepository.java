1f9c7c6b7a98c2e3ebb13f73bcd3f51470a1e8a4
rafael-vincensi <rafaelvincensi.m@gmail.com>
2026-09-03 19:31:52 -0300

    feat(groups): adiciona metodo de busca por codigo

--- src/main/java/com/gitrats/groups/GroupsRepository.java
+++ src/main/java/com/gitrats/groups/GroupsRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface GroupsRepository extends JpaRepository <Groups, Long> {
+    Optional<Groups> findByCode(String code);
 }
