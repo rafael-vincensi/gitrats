be95ff04801027f2a010e02bb810320d504a69ce
rafael-vincensi <rafaelvincensi.m@gmail.com>
2026-09-04 22:09:08 -0300

    fix(groups): atualiza repository de group member

--- src/main/java/com/gitrats/groups/GroupMemberRepository.java
+++ src/main/java/com/gitrats/groups/GroupMemberRepository.java
@@ -1,10 +1,9 @@
 package com.gitrats.groups;
 
+import com.gitrats.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface GroupMemberRepository extends JpaRepository <GroupMember, Long> {
-    Optional<GroupMember> findByName(String name);
-
+    Optional<GroupMember> findByUserAndGroup(User user, Group group);
 }
