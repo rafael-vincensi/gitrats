d03eae36145381b213e5129c23b5bf57b545d31c
rafael-vincensi <rafaelvincensi.m@gmail.com>
2026-09-04 22:20:03 -0300

    fix(user): corrige query para busca de nome na search bar

--- src/main/java/com/gitrats/user/UserRepository.java
+++ src/main/java/com/gitrats/user/UserRepository.java
@@ -6,5 +6,5 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository <User, Long> {
-    Optional<User> findByUsername(String username);
+    List<User> findByUsernameContainingIgnoreCaseOrDisplayNameContainingIgnoreCase (String username, String displayName);
 }
